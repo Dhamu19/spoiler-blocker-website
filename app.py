7b3b355ef88ed3c035e2747b7134601e2e86824d
varung97 <varung97@yahoo.co.in>
2016-07-11 17:03:33 +0530

    Added heroku requirements and basic app

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, send_from_directory, request
+import os
+from data import write_img
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+# @app.route('/img')
+# def images():
+#     return send_from_directory('static/img', "main.jpg")
+#
+# @app.route('/data', methods=['POST'])
+# def py_file():
+#     filedata = request.files['file']
+#     return write_img(filedata)
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
---
+++ static/js/app.js
@@ -0,0 +1,17 @@
+var app = angular.module('SpoilerBlockerWebsite', []);
+
+app.controller('MainController', function($scope, $http) {
+	// $scope.scrapePic = function() {
+	// 	$http({
+	// 		method: 'POST',
+ // 			url: '/data',
+ // 			data: fd,
+	// 		headers: {'Content-Type': undefined},
+	//     transformRequest: angular.identity
+	// 	}).then(function(data) {
+	// 		$scope.flag = true;
+	// 		$scope.movieData = data;
+	// 		console.log(data);
+	// 	});
+	// };
+});
