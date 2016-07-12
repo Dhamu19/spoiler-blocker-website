15dfa317e867025ff52e9907dd97a3d1ac381064
varung97 <varung97@yahoo.co.in>
2016-07-12 12:25:12 +0530

    Added database

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 from flask import Flask, render_template, send_from_directory, request
 import os
 import json
+import psycopg2
+import psycopg2.extras
+import urlparse
+
+urlparse.uses_netloc.append("postgres")
+url = urlparse.urlparse(os.environ["DATABASE_URL"])
+
+conn = psycopg2.connect(
+    database=url.path[1:],
+    user=url.username,
+    password=url.password,
+    host=url.hostname,
+    port=url.port
+)
+cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
 app = Flask(__name__)
 
@@ -14,10 +29,14 @@ def index():
 #
 @app.route('/testRoute', methods=['POST'])
 def test():
-    data = json.loads(request.data.decode())
-    testData = data['testData']
-    return testData
+    # data = json.loads(request.data.decode())
+    # testData = data['testData']
+    # return testData
+    cur.execute("SELECT * FROM test_table;")
+    return cur.fetchone()['text']
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
+    cur.close()
+    conn.close()
